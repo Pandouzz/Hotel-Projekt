f659363c016705377b4b75484411290906a271fd
charlybahr <charlyxxxddd@gmail.com>
2025-06-11 15:42:49 +0200

    aaaaa

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Room } from '../room';
+import { Roomservice } from '../roomservice';
 
 @Component({
   selector: 'app-details',
