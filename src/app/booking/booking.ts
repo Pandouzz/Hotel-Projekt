a618015a21866966fbd2961542b9af6f8fc9a906
charlybahr <charlyxxxddd@gmail.com>
2025-06-12 12:44:50 +0200

    Zurück Button bei Buchung

--- src/app/booking/booking.ts
+++ src/app/booking/booking.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -10,25 +10,25 @@ import { BookingService } from './buchungsservice';
 import { Roomservice } from '../roomservice';
 import { Room } from '../room';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-booking',
-  imports: [ReactiveFormsModule, MatInputModule, MatSelectModule, MatCardModule, MatButtonModule, CommonModule, FormsModule, ReactiveFormsModule,],
+  imports: [ReactiveFormsModule, MatInputModule, MatSelectModule, MatCardModule, MatButtonModule, CommonModule, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './booking.html',
   styleUrl: './booking.css'
 })
 export class Booking implements OnInit {
   selectedRoom: Room | undefined;
   bookingForm: FormGroup;
+  route: ActivatedRoute = inject(ActivatedRoute);
 
   constructor(
     private fb: FormBuilder,
     private bookingService: BookingService,
-    private roomService: Roomservice,
-    private route: ActivatedRoute
-  ) {
+    private roomService: Roomservice  ) {
     this.selectedRoom = this.roomService.getRoomByNumber(Number(this.route.snapshot.params['roomNumber']));
     this.bookingForm = this.fb.group({
       firstName: ['', Validators.required],
@@ -36,7 +36,8 @@ export class Booking implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       phoneNumber: ['', Validators.required],
       checkInDate: [null, Validators.required],
-      checkOutDate: [null, Validators.required]
+      checkOutDate: [null, Validators.required],
+      anzPersonen: [null, [Validators.required, Validators.min(1)]],
     });
   }
 
